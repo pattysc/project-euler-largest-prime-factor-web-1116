0d6557260647faf58c22eecbca6b1a7484802f7d
pattysc <pattys@users.noreply.github.com>
2017-02-03 09:21:25 -0500

    Done.

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,29 @@
 # Enter your procedural solution here!
+def isprime?(n)
+  return false if n < 2;
+
+  index = 2
+  while index < number
+    if (number % index) == 0
+      return false
+    end
+    index += 1
+  end
+
+  return true
+end
+
+
+def largest_prime_factor(n)
+  index = n-1
+
+  while index > 0
+    if n%index  == 0
+      if isprime?(index)
+        return index
+      end
+    end
+    index -= 1
+  end
+
+end
